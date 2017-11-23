49ac4ae26c3243906d8f6c9dd5d146f0e3855d31
zhangxi <zhangxi@xingshulin.com>
2017-11-23 11:08:29 +0800

    [zhangxi] 添加 tool文件

---
+++ tool/tr.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "HELLO wHO " | tr 'A-Z' 'a-z'
+
+echo 12345 | tr '0-9' '9876543210'
+echo 87654 | tr '9876543210' '0-9'
+
+echo "tr came , tr saw , tr conquered" | tr 'a-zA-Z' 'n-za-mN-ZA-M'
+echo "ge pnzr , ge fnj , ge pbadhrerq" | tr 'a-zA-Z' 'n-za-mN-ZA-M'
+
+# tr '\t' ' ' < ../data/files.txt
+
+echo "hello 123 world 456" | tr -d '0-9'
+echo hello 1 char 2 next 4 |tr -d -c '0-9 \n'
+echo "GUN is     1    not           UNIX.Reac" | tr -s ' '
+
+cat ../data/example.txt | xargs | echo $[ $(tr ' ' '+')]
+
+echo "line1 \n \nline2 " | tr -s '\n' 
+
+# alnum
+# alpha
+# cntrl
+# digit
+# graph
+# lower
+# print
+# punct
+# upper
+# xdigit
+
+# echo "#!/bin/bash" > md5.sh
---
+++ tool/xargs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cat ../data/example.txt | xargs
+
+# -n 指定参数数量 将来自stdin的文本划分成多行
+cat ../data/example.txt | xargs -n 3
+
+#
+# echo "splitXsplitXsplitXsplitX" | xargs -d X
+
+cat ../data/args.txt | xargs  -I {} echo {} '#' 
+
+# find .. -type f -name "*.txt" -print0 | xargs -0 echo
+
+# find .. -type f -name "*.sh" -print0 | xargs -0 wc -l
+
+
+cat ../data/files.txt | (while read arg ; do cat "../data/$arg"; done)
+
+cat ../data/files.txt | xargs -I {} cat ../data/{}
+
+# echo "#!/bin/bash" > sort.sh
+ 
